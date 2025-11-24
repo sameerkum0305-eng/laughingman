3ea8ee7faf6b12d18df374da7b3d62249543995b
sameerkum0305-eng <sameerkum0305@gmail.com>
2025-11-24 10:38:21 -0500

    added random color function

--- laughingman.js
+++ laughingman.js
@@ -22,6 +22,31 @@ document.addEventListener('DOMContentLoaded', function(){
    squareArray.forEach(function(element,index){
      var dx = SPEED * (Math.random()*2 -1);
      var dy = SPEED * (Math.random()*2 -1);
+setInterval(() => {
+    let currentLeft = parseInt(element.style.left) || 225;
+    let currentTop = parseInt(element.style.top) || 175;
+    if (currentLeft >= 450 || currentLeft <= 0) {
+        dx *= -1;
+        element.style.borderColor = getRandomColor();
+    }
+    if (currentTop >= 350 || currentTop <= 0) {
+        dy *= -1;
+        element.style.borderColor = getRandomColor();
+    }
+
+    element.style.left = (currentLeft + dx) + "px";
+    element.style.top = (currentTop + dy) + "px";
+}, TIMER_SPEED);
+
+function getRandomColor() {
+    let r = parseInt(Math.random() * 256);
+    let g = parseInt(Math.random() * 256);
+    let b = parseInt(Math.random() * 256);
+    let color = `rgb(${r},${g},${b})`;
+    return color;
+}
+
+     
      
 });
 });
