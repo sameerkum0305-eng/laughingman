d40dbba20a4b99d9a6da1345ef20261bbcd35992
sameerkum0305-eng <sameerkum0305@gmail.com>
2025-12-02 15:40:11 -0500

    Made sure everything was copied from class

--- laughingman.js
+++ laughingman.js
@@ -1,42 +1,51 @@
-document.addEventListener('DOMContentLoaded', function(){
-  let squareElement = document.getElementById("square");
-  
-  squareElement.addEventListener("click", ()=>) {
-    alert("OMG YOU CAUGHT ME!");
-  });
-  
-  let box = document.querySelector("#box");
-  
-  for(let i = 0; i<SQUARE_COUNT; i++)
- {
-   
-   let square = document.createElement('img');
-   square.src = 'laughing.man.jpg';
-   square.alt = 'Catch the Laughing Man!';
-   square.className = 'square';
-   box.appendChild(square);
-   //add it to the screen
-   } 
-   let allSquares = box.children;
-   let squareArray = Array.from(children);
-   squareArray.forEach(function(element,index){
-     var dx = SPEED * (Math.random()*2 -1);
-     var dy = SPEED * (Math.random()*2 -1);
-setInterval(() => {
-    let currentLeft = parseInt(element.style.left) || 225;
-    let currentTop = parseInt(element.style.top) || 175;
-    if (currentLeft >= 450 || currentLeft <= 0) {
-        dx *= -1;
-        element.style.borderColor = getRandomColor();
-    }
-    if (currentTop >= 350 || currentTop <= 0) {
-        dy *= -1;
-        element.style.borderColor = getRandomColor();
+const SQUARE_COUNT = 30;
+const SPEED = 10;
+const TIMER_SPEED = 200;
+
+document.addEventListener('DOMContentLoaded', () => {
+    let squareElement = document.getElementById("square");
+    //let squareElement = document.querySelector("#square");
+    squareElement.addEventListener("click", () => {
+        alert("OMG YOU CAUGHT ME!");
+    });
+
+    let box = document.querySelector("#box");
+
+    for (let i = 0; i < SQUARE_COUNT; i++) {
+        //create the square
+        let square = document.createElement('img');
+        square.src = 'laughing_man.jpg';
+        square.alt = 'Catch the Laughing Man!';
+        square.className = 'square';
+        box.appendChild(square);
+        //add it to the screen
     }
 
-    element.style.left = (currentLeft + dx) + "px";
-    element.style.top = (currentTop + dy) + "px";
-}, TIMER_SPEED);
+    let allSquares = box.children;
+    let squareArray = Array.from(allSquares);
+    squareArray.forEach(function (element, index) {
+        var dx = SPEED * Math.random() * 2 - 1;
+        var dy = SPEED * Math.random() * 2 - 1;
+
+        setInterval(() => {
+            let currentLeft = parseInt(element.style.left) || 225;
+            let currentTop = parseInt(element.style.top) || 175;
+
+            if (currentLeft >= 450 || currentLeft <= 0) {
+                dx *= -1;
+                element.style.borderColor = getRandomColor();
+            }
+
+            if (currentTop >= 350 || currentTop <= 0) {
+                dy *= -1;
+                element.style.borderColor = getRandomColor();
+            }
+
+            element.style.left = (currentLeft + dx) + "px";
+            element.style.top = (currentTop + dy) + "px";
+        }, TIMER_SPEED);
+    });
+});
 
 function getRandomColor() {
     let r = parseInt(Math.random() * 256);
@@ -45,8 +54,3 @@ function getRandomColor() {
     let color = `rgb(${r},${g},${b})`;
     return color;
 }
-
-     
-     
-});
-});
