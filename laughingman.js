f5f362396ac89741f71dde1040ec65a5a86ec8ce
sameerkum0305-eng <sameerkum0305@gmail.com>
2025-12-02 18:18:19 -0500

    updated laughingman.js with all features to make sure everything is organized

--- laughingman.js
+++ laughingman.js
@@ -10,6 +10,7 @@ function getColor() {
 }
 
 document.addEventListener('DOMContentLoaded', () => {
+
     let squareElement = document.getElementById("square");
     //let squareElement = document.querySelector("#square");
     squareElement.addEventListener("click", () => {
@@ -20,84 +21,70 @@ document.addEventListener('DOMContentLoaded', () => {
 
     for (let i = 0; i < SQUARE_COUNT; i++) {
         //create the square
-          for (let i = 0; i < SQUARE_COUNT; i++) {
         let img = document.createElement("img");
         img.className = "square";
         img.src = "laughing_man.jpg";
         box.appendChild(img);
-    }
         //add it to the screen
     }
 
     let allSquares = box.children;
     let squareArray = Array.from(allSquares);
-    squareArray.forEach(function (element, index) {
+
+    squareArray.forEach(function (element) {
+
         var dx = SPEED * Math.random() * 2 - 1;
         var dy = SPEED * Math.random() * 2 - 1;
 
         setInterval(() => {
+
             let currentLeft = parseInt(element.style.left) || 225;
             let currentTop = parseInt(element.style.top) || 175;
 
             if (currentLeft >= 450 || currentLeft <= 0) {
 
                 //change the border and background as hits wall feature
-                   dx *= -1;
-                square.style.borderColor = getColor();
-                square.style.backgroundColor = getColor();
-            }
-
-            if (currentTop >= 350 || currentTop <= 0) {
-                dy *= -1;
-                square.style.borderColor = getColor();
-                square.style.backgroundColor = getColor();
-            }
-
                 dx *= -1;
-                element.style.borderColor = getRandomColor();
+                element.style.borderColor = getColor();
+                element.style.backgroundColor = getColor();
             }
 
             if (currentTop >= 350 || currentTop <= 0) {
                 dy *= -1;
-                element.style.borderColor = getRandomColor();
+                element.style.borderColor = getColor();
+                element.style.backgroundColor = getColor();
             }
 
             element.style.left = (currentLeft + dx) + "px";
             element.style.top = (currentTop + dy) + "px";
+
         }, TIMER_SPEED);
     });
-});
 
- box.addEventListener("mouseover", function(event) {
+    box.addEventListener("mouseover", function(event) {
         let t = event.target;
 
-     //square changing background, not image
+        //square changing background, not image
         if (t.id === "square") {
             t.style.backgroundColor = getColor();
-        } 
-      //change to orochimaru.jpg
+        }
+        //change to orochimaru.jpg
         else if (t.tagName === "IMG") {
             t.src = "Orochimaru.jpg";
         }
     });
 
-function getRandomColor() {
-    let r = parseInt(Math.random() * 256);
-    let g = parseInt(Math.random() * 256);
-    let b = parseInt(Math.random() * 256);
-    let color = `rgb(${r},${g},${b})`;
-    return color;
-}
-
- box.addEventListener("mouseout", function(event) {
+    box.addEventListener("mouseout", function(event) {
         let t = event.target;
-//reset square background
+
+        //reset square background
         if (t.id === "square") {
             t.style.backgroundColor = "red";
-        } 
-
-            //change image back to laughing man
+        }
+        //change image back to laughing man
         else if (t.tagName === "IMG") {
             t.src = "laughing_man.jpg";
         }
     });
+
+});
